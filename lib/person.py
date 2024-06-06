42b1c554be5beef80490997471ec496e251db0f9
MutaiNewton56 <mutainewton56.gmail.com>
2024-06-06 18:30:20 +0300

    instance methods

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,13 @@
-#!/usr/bin/env python3
-
 class Person:
-    # Class body goes here
+    def __init__(self, name="Unknown", age=0):
+        self.name = name
+        self.age = age
 
-    #Instance method definition
-    pass
+    def introduce(self):
+        print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
